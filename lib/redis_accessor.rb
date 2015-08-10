37243869b8dc8f12165d255a781ee54b9fa1ea9a
CDR <cdr@CDRs-iMac.local>
2015-08-10 13:14:36 -0400

    loader module

---
+++ lib/redis_accessor.rb
@@ -0,0 +1,14 @@
+require 'redis'
+require 'json'
+require 'launchy'
+
+require_relative 'redis_accessor/accessor.rb'
+require_relative 'redis_accessor/lesson.rb'
+require_relative 'redis_accessor/mod.rb'
+require_relative 'redis_accessor/teacher_resources.rb'
+require_relative 'redis_accessor/template.rb'
+require_relative 'redis_accessor/unit.rb'
+
+
+module RedisAccessor
+end
